2d16a0c44717a5090d14b1f432409519fa60214f
Menon <menonpranto@gmail.com>
2025-04-21 13:01:06 +0600

    test rlf thresh

--- notebooks/tests/test_MRO_library.py
+++ notebooks/tests/test_MRO_library.py
@@ -1,6 +1,9 @@
 from notebooks.MRO_library import *
+from notebooks.MRO_library import _check_rlf_threshold
 import unittest
 import pandas as pd
+import numpy as np
+import pytest
 
 class TestMROLibrary(unittest.TestCase):
     data = {
@@ -21,6 +24,70 @@ class TestMROLibrary(unittest.TestCase):
     def test_calculate_mro_metric(self):
         result = calculate_mro_metric(self.df)
         self.assertEqual(result, 1.85)
+
+    def test_haversine(self):
+        lat1, lon1 = -90, -180
+        lat2, lon2 = 90, 180
+        expected_distance = 20037.508342789242
+        result = haversine(lat1, lon1, lat2, lon2)
+        self.assertAlmostEqual(result, expected_distance, places=2)
+
+    def test_rlf_threshold(self):
+        # Dummy Data
+        data_current_tick = pd.DataFrame({
+            'ue_id':       [1, 1, 2, 2, 3, 3],
+            'cell_id':          [1, 2, 1, 2, 1, 2],
+            'cell_rxpower_dbm': [68, 76, 40, 74, 91, 102],
+            'sinr_db':          [30, 32, 20, 23, 26, 23],
+            # Rest is Dummy Data
+            'cell_lat':         [10, 20, 30, 40, 50, 60],
+            'cell_lon':         [10, 20, 30, 40, 50, 60],
+            'cell_carrier_freq_mhz': [20, 20, 20, 20, 20, 20],
+            'cell_az_deg':      [10, 20, 30, 40, 50, 60],
+            'distance_km':      [10, 20, 30, 40, 50, 60],
+            'relative_bearing': [10, 20, 30, 40, 50, 60]
+        })
+
+        df = pd.DataFrame({
+            'ue_id':       [1, 2, 3],
+            'cell_id':          [2, 1, 2],
+            'cell_rxpower_dbm': [76, 40, 102],
+            'sinr_db':          [32, 20, 23],
+            # Rest is Dummy Data
+            'cell_lat':         [10, 20, 30],
+            'cell_lon':         [10, 20, 30],
+            'cell_carrier_freq_mhz': [20, 20, 20],
+            'cell_az_deg':      [10, 20, 30],
+            'distance_km':      [10, 20, 30],
+            'relative_bearing': [10, 20, 30]
+        })
+
+        answer = _check_rlf_threshold(df, data_current_tick, 25)
+
+        result = {
+        'ue_id': [1, 2, 3],
+        'cell_id': [2, 'RLF', 1],
+        'cell_rxpower_dbm': [76.0, '-inf', 91.0],
+        'sinr_db': [32.0, '-inf', 26.0],
+        'cell_lat': [10, 20, 50],
+        'cell_lon': [10, 20, 50],
+        'cell_carrier_freq_mhz': [20, 20, 20],
+        'cell_az_deg': [10, 20, 50],
+        'distance_km': [10, 20, 50],
+        'relative_bearing': [10, 20, 50]
+        }
+        result = pd.DataFrame(result)
+
+        pd.set_option('display.max_rows', None)  # Display all rows
+        pd.set_option('display.max_columns', None)  # Display all columns
+        pd.set_option('display.width', None)  # No line width limit
+        pd.set_option('display.max_colwidth', None)  # No limit on column width
+        
+        # Convert the columns to float64 in the result DataFrame
+        result['cell_rxpower_dbm'] = pd.to_numeric(result['cell_rxpower_dbm'], errors='coerce')
+        result['sinr_db'] = pd.to_numeric(result['sinr_db'], errors='coerce')
+        
+        self.assertTrue(answer.equals(result))
 
 if __name__ == '__main__':
     unittest.main()
